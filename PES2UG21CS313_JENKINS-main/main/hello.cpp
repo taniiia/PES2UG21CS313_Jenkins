31f428675f4e123094731d4f869e2b8cc85a0041
taniiia <121872044+taniiia@users.noreply.github.com>
2024-03-06 17:50:56 +0530

    Update hello.cpp

--- PES2UG21CS313_JENKINS-main/main/hello.cpp
+++ PES2UG21CS313_JENKINS-main/main/hello.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello, Jenkins\n";
-  cout << "Hello, Jenkins\n";
   cout << "Task 5\n";
   cout << "I have successfully built and run\n";
   return 0;
